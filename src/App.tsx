ae8e7cbe2f4e55089d957907deccd40d3ea9d333
Mack Cooper <mackcooper1408@gmail.com>
2023-11-12 10:54:46 -0800

    update donate button color

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import NavBar from "./components/NavBar";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Welcome from "./pages/Wecome";
-import "./App.css";
-import { useRef } from "react";
-import WhoAreWe from "./pages/WhoAreWe";
-import PartnerForProgress from "./pages/PartnerForProgress";
-import Stories from "./pages/Stories";
-import HealthImpact from "./pages/HealthImpact";
+import NavBar from './components/NavBar';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Welcome from './pages/Wecome';
+import './App.css';
+import { useRef } from 'react';
+import WhoAreWe from './pages/WhoAreWe';
+import PartnerForProgress from './pages/PartnerForProgress';
+import Stories from './pages/Stories';
+import HealthImpact from './pages/HealthImpact';
 
 function App() {
   const education = useRef(null);
@@ -19,19 +19,14 @@ function App() {
       <NavBar />
       <main>
         <Welcome />
-        <WhoAreWe
-          educationRef={education}
-          healthRef={health}
-          nutritionRef={nutrition}
-        />
+        <WhoAreWe educationRef={education} healthRef={health} nutritionRef={nutrition} />
         <section ref={education} className="bg-light text-dark">
           <Container>
             <h2>How your contributions affect EDUCATION</h2>
             <p className="fs-5">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic,
-              unde tempora eos fugit error in perferendis veniam? Facere, quo
-              dolores, expedita iure tempore asperiores voluptatum esse est modi
-              ut aut.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, unde tempora eos fugit
+              error in perferendis veniam? Facere, quo dolores, expedita iure tempore asperiores
+              voluptatum esse est modi ut aut.
             </p>
           </Container>
         </section>
@@ -40,10 +35,9 @@ function App() {
           <Container>
             <h2>How your contributions affect NUTRITION</h2>
             <p className="fs-5">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic,
-              unde tempora eos fugit error in perferendis veniam? Facere, quo
-              dolores, expedita iure tempore asperiores voluptatum esse est modi
-              ut aut.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, unde tempora eos fugit
+              error in perferendis veniam? Facere, quo dolores, expedita iure tempore asperiores
+              voluptatum esse est modi ut aut.
             </p>
           </Container>
         </section>
@@ -51,10 +45,11 @@ function App() {
         <Stories />
       </main>
       <Button
+        className="tw-bg-[#25348F] tw-text-white tw-rounded-3xl tw-px-10 tw-border-0"
         href="https://www.nalamindia.org/donate"
         variant="outline-success"
         size="lg"
-        style={{ position: "fixed", right: 10, bottom: 10, zIndex: 999 }}
+        style={{ position: 'fixed', right: 20, bottom: 20, zIndex: 999 }}
       >
         Donate
       </Button>
