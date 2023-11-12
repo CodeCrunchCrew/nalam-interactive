9d6c3574c278d9b055b96c53f66b6b9d9bc62e4d
acidicNic <nicole.r.abadie@gmail.com>
2023-11-12 13:06:48 -0800

    stories, popup modal.

--- src/pages/Stories/index.tsx
+++ src/pages/Stories/index.tsx
@@ -12,31 +12,31 @@ const Stories = () => {
         <div className="d-flex flex-column justify-content-center align-items-center" style={{ background: "#C9CCE3",  height: "90vh"}}>
           <Row className="d-flex h-100 justify-content-center align-items-center py-4">
             <Col className="col-4">
-              <div className="image-container mx-4 rounded-5" style={{position: 'relative'}}>
-                <img src={girl0} className="rounded-5" style={{ boxShadow: '20px 20px 0px #25348F' }} />
-                <div className="overlay-text rounded-5 h-100 w-100" style={{ background: 'rgba(255, 255, 255, 0.5)', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-                  <div className="h-100" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
-                    <h1 className="">Name, since 2020</h1>
+              <div className="image-container mx-4" style={{position: 'relative', borderRadius: '40px'}}  data-bs-toggle="modal" data-bs-target="#girlInfoModal">
+                <img src={girl0} className="" style={{ borderRadius: '40px', boxShadow: '20px 20px 0px #25348F' }} />
+                <div className="overlay-text h-100 w-100" style={{ borderRadius: '40px', background: 'rgba(255, 255, 255, 0.5)', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+                  <div className="h-100 align-content-center text-center" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
+                    <h1 className="m-3">Name, since 2023</h1>
                   </div>
                 </div>
               </div>
             </Col>
             <Col className="col-4">
-              <div className="image-container mx-4 rounded-5" style={{position: 'relative'}}>
-                <img src={girl1} className="rounded-5" style={{ boxShadow: '20px 20px 0px #25348F' }} />
-                <div className="overlay-text rounded-5 h-100 w-100" style={{ background: 'rgba(255, 255, 255, 0.5)', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-                  <div className="h-100" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
-                    <h1 className="">Name, since 2020</h1>
+              <div className="image-container mx-4" style={{position: 'relative', borderRadius: '40px',}}  data-bs-toggle="modal" data-bs-target="#girlInfoModal">
+                <img src={girl1} className="" style={{ boxShadow: '20px 20px 0px #25348F', borderRadius: '40px', }} />
+                <div className="overlay-text h-100 w-100" style={{ borderRadius: '40px', background: 'rgba(255, 255, 255, 0.5)', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+                  <div className="h-100 align-content-center text-center" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
+                    <h1 className="m-3">Rekha, since 2015</h1>
                   </div>
                 </div>
               </div>
             </Col>
             <Col className="col-4">
-              <div className="image-container mx-4 rounded-5" style={{position: 'relative'}}>
-                <img src={girl2} className="rounded-5" style={{ boxShadow: '20px 20px 0px #25348F' }} />
-                <div className="overlay-text rounded-5 h-100 w-100" style={{ background: 'rgba(255, 255, 255, 0.5)', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-                  <div className="h-100" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
-                    <h1 className="">Name, since 2020</h1>
+              <div className="image-container mx-4" style={{position: 'relative', borderRadius: '40px' }}  data-bs-toggle="modal" data-bs-target="#girlInfoModal">
+                <img src={girl2} className="" style={{ boxShadow: '20px 20px 0px #25348F', borderRadius: '40px' }} />
+                <div className="overlay-text h-100 w-100" style={{ borderRadius: '40px', background: 'rgba(255, 255, 255, 0.5)', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+                  <div className="h-100 align-content-center text-center" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
+                    <h1 className="m-3">Name, since 2023</h1>
                   </div>
                 </div>
               </div>
@@ -44,6 +44,33 @@ const Stories = () => {
           </Row>
         </div>
       </Col>
+      
+      <div className="modal fade" id="girlInfoModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
+          <div className="modal-content" style={{ borderRadius: '42px', background: "#25348F" }}>
+            <div className="w-100 p-0 m-0 d-flex">
+              <div>
+                <img src={girl1} className="" style={{ borderRadius: "0px", boxShadow: '20px 20px 0px #25348F' }} />
+              </div>
+              <div className="d-flex flex-column m-4 w-100">
+                <h1 className="fs-3 p-3 rounded-pill text-light" style={{ background: "#829ED5" }}>Rekha</h1>
+                <div className="align-self-end align-self-baseline p-3">
+                  <p className="fs-5 text-light">
+                    <b>Age:</b> 9
+                  </p>
+                  <p className="fs-5 text-light">
+                    <b>Her Story:</b> Rekha is a half-orphan living at the orphanage since the tragic loss of her grandmother. With her father having passed away and her mother remarried, Rekha faced the harsh reality of neglect and starvation. A village elder intervened at this crucial point, leading her to the safety of our orphanage. Admitted here, Rekha encountered a nurturing environment that offered her opportunities she never dreamt possible.Your contributions play a significant role in transforming lives like Rekha's, providing hope and paving the path for a brighter future.
+                  </p>
+                  <div className="d-flex justify-content-end align-content-end">
+                    <button type="button" className="btn btn-lg btn-outline-light mt-auto ml-auto align-self-end rounded-pill" data-bs-dismiss="modal" aria-label="Close">Back</button> 
+                  </div>
+               </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </section>
   );
 };
