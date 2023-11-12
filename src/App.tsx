5a9244f70a710238d3542dfd4b8bd919cd027733
Mack Cooper <mackcooper1408@gmail.com>
2023-11-11 18:00:28 -0800

    remove boiler plate css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useRef } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import { useRef } from 'react';
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
 
-import "./App.css";
+import './App.css';
 
 function App() {
   const ref = useRef(null);
@@ -16,14 +14,8 @@ function App() {
     <>
       <LocomotiveScrollProvider options={options} containerRef={ref}>
         <main ref={ref}>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
+          <h1 className="my-0 mx-auto">Main</h1>
         </main>
-        <h1>Vite + React</h1>
       </LocomotiveScrollProvider>
     </>
   );
