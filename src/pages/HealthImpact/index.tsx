d290628fd4c26b327b859778a8d602ea1062a1c4
samau3 <samau415@gmail.com>
2023-11-12 11:52:41 -0800

    Update numbers regarding impact

--- src/pages/HealthImpact/index.tsx
+++ src/pages/HealthImpact/index.tsx
@@ -1,5 +1,5 @@
-import IndiaMap from '../../assets/indiamap.jpg';
-import { Container } from 'react-bootstrap';
+import IndiaMap from "../../assets/indiamap.jpg";
+import { Container } from "react-bootstrap";
 
 interface HealthImpactProps {
   healthRef: React.RefObject<HTMLElement>;
@@ -12,47 +12,47 @@ const HealthImpact = ({ healthRef }: HealthImpactProps) => {
       className="education bg-light text-dark"
       style={{
         backgroundImage: `url(${IndiaMap})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        height: '100vh',
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
       }}
     >
       <Container
         style={{
-          backdropFilter: 'blur(1px)',
-          height: '100%',
-          position: 'relative',
+          backdropFilter: "blur(1px)",
+          height: "100%",
+          position: "relative",
         }}
       >
         <div
           style={{
-            position: 'absolute',
-            display: 'flex',
-            top: '7%',
-            right: '10px',
-            width: '70%',
-            backgroundColor: '#829ED5',
-            borderRadius: '20px',
+            position: "absolute",
+            display: "flex",
+            top: "7%",
+            right: "10px",
+            width: "70%",
+            backgroundColor: "#829ED5",
+            borderRadius: "20px",
           }}
         >
           <div
             style={{
-              backgroundColor: '#5C67AB',
-              borderTopLeftRadius: '20px',
-              borderBottomLeftRadius: '20px',
-              alignItems: 'center',
-              display: 'flex',
-              padding: '10px',
+              backgroundColor: "#5C67AB",
+              borderTopLeftRadius: "20px",
+              borderBottomLeftRadius: "20px",
+              alignItems: "center",
+              display: "flex",
+              padding: "10px",
             }}
           >
             <h3
               style={{
-                fontSize: '70px',
-                fontWeight: '400',
-                lineHeight: '106px',
-                letterSpacing: '0em',
-                textAlign: 'center',
+                fontSize: "70px",
+                fontWeight: "400",
+                lineHeight: "106px",
+                letterSpacing: "0em",
+                textAlign: "center",
               }}
             >
               $5,780
@@ -60,70 +60,72 @@ const HealthImpact = ({ healthRef }: HealthImpactProps) => {
           </div>
           <div
             style={{
-              alignItems: 'center',
-              display: 'flex',
+              alignItems: "center",
+              display: "flex",
             }}
           >
             <p
               style={{
-                color: 'white',
+                color: "white",
                 margin: 0,
               }}
             >
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla dicta nam sed nemo,
-              sequi quidem illum cumque vitae, molestiae facere quam voluptates deserunt, ipsam sunt
-              non tempore suscipit incidunt ex.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
+              dicta nam sed nemo, sequi quidem illum cumque vitae, molestiae
+              facere quam voluptates deserunt, ipsam sunt non tempore suscipit
+              incidunt ex.
             </p>
           </div>
         </div>
         <div
           style={{
-            position: 'absolute',
-            display: 'flex',
-            bottom: '20%',
-            left: '10px',
-            width: '70%',
-            backgroundColor: '#829ED5',
-            borderRadius: '20px',
+            position: "absolute",
+            display: "flex",
+            bottom: "20%",
+            left: "10px",
+            width: "70%",
+            backgroundColor: "#829ED5",
+            borderRadius: "20px",
           }}
         >
           <div
             style={{
-              backgroundColor: '#5C67AB',
-              borderTopLeftRadius: '20px',
-              borderBottomLeftRadius: '20px',
-              alignItems: 'center',
-              display: 'flex',
-              padding: '10px',
+              backgroundColor: "#5C67AB",
+              borderTopLeftRadius: "20px",
+              borderBottomLeftRadius: "20px",
+              alignItems: "center",
+              display: "flex",
+              padding: "10px",
             }}
           >
             <h3
               style={{
-                fontSize: '70px',
-                fontWeight: '400',
-                lineHeight: '106px',
-                letterSpacing: '0em',
-                textAlign: 'center',
+                fontSize: "70px",
+                fontWeight: "400",
+                lineHeight: "106px",
+                letterSpacing: "0em",
+                textAlign: "center",
               }}
             >
-              ~$20,000
+              ~$21,000
             </h3>
           </div>
           <div
             style={{
-              alignItems: 'center',
-              display: 'flex',
+              alignItems: "center",
+              display: "flex",
             }}
           >
             <p
               style={{
-                color: 'white',
+                color: "white",
                 margin: 0,
               }}
             >
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla dicta nam sed nemo,
-              sequi quidem illum cumque vitae, molestiae facere quam voluptates deserunt, ipsam sunt
-              non tempore suscipit incidunt ex.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
+              dicta nam sed nemo, sequi quidem illum cumque vitae, molestiae
+              facere quam voluptates deserunt, ipsam sunt non tempore suscipit
+              incidunt ex.
             </p>
           </div>
         </div>
