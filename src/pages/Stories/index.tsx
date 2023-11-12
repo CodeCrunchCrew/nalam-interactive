cb0eb5bf50c78ab9945772c07d9ea2d4b7c7e4f1
acidicNic <nicole.r.abadie@gmail.com>
2023-11-12 12:09:26 -0800

    stories hover

--- src/pages/Stories/index.tsx
+++ src/pages/Stories/index.tsx
@@ -1,23 +1,48 @@
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import verticalPlaceholderImg from '../../assets/placeholder-tall.png';
+import girl0 from '../../assets/girl-0.png';
+import girl1 from '../../assets/girl-1.png';
+import girl2 from '../../assets/girl-2.png';
 
 const Stories = () => {
   return (
-    <section className="bg-light text-dark">
+    <section className="col bg-light text-dark mb-0 pb-0">
       <Col>
         <h1 className="display-5 mb-4">A Glimpse into the Lives We’ve Changed</h1>
-        <Row className="d-flex justify-content-center mx-4">
-          <Col className="col-4">
-            <img src={verticalPlaceholderImg} className="rounded w-75" />
-          </Col>
-          <Col className="col-4">
-            <img src={verticalPlaceholderImg} className="rounded w-75" />
-          </Col>
-          <Col className="col-4">
-            <img src={verticalPlaceholderImg} className="rounded w-75" />
-          </Col>
-        </Row>
+        <div className="d-flex flex-column justify-content-center align-items-center" style={{ background: "#C9CCE3",  height: "90vh"}}>
+          <Row className="d-flex h-100 justify-content-center align-items-center py-4">
+            <Col className="col-4">
+              <div className="image-container mx-4 rounded-5" style={{position: 'relative'}}>
+                <img src={girl0} className="rounded-5" style={{ boxShadow: '20px 20px 0px #25348F' }} />
+                <div className="overlay-text rounded-5 h-100 w-100" style={{ background: 'rgba(255, 255, 255, 0.5)', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+                  <div className="h-100" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
+                    <h1 className="">Name, since 2020</h1>
+                  </div>
+                </div>
+              </div>
+            </Col>
+            <Col className="col-4">
+              <div className="image-container mx-4 rounded-5" style={{position: 'relative'}}>
+                <img src={girl1} className="rounded-5" style={{ boxShadow: '20px 20px 0px #25348F' }} />
+                <div className="overlay-text rounded-5 h-100 w-100" style={{ background: 'rgba(255, 255, 255, 0.5)', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+                  <div className="h-100" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
+                    <h1 className="">Name, since 2020</h1>
+                  </div>
+                </div>
+              </div>
+            </Col>
+            <Col className="col-4">
+              <div className="image-container mx-4 rounded-5" style={{position: 'relative'}}>
+                <img src={girl2} className="rounded-5" style={{ boxShadow: '20px 20px 0px #25348F' }} />
+                <div className="overlay-text rounded-5 h-100 w-100" style={{ background: 'rgba(255, 255, 255, 0.5)', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+                  <div className="h-100" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
+                    <h1 className="">Name, since 2020</h1>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </div>
       </Col>
     </section>
   );
