5a961f6625141ed68ced1dff936359e87c1682ea
acidicNic <nicole.r.abadie@gmail.com>
2023-11-11 17:50:06 -0800

    navbar added with logo only

---
+++ src/NavBar.tsx
@@ -0,0 +1,18 @@
+function NavBar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <div className="navbar-brand">
+            <img src="https://assets-global.website-files.com/651eac31835f453a27bb7c5a/651eaf8dda4d313d93565f20_Nalam%20Logo.svg" loading="lazy" width="87" height="23" alt=""></img>
+          </div>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </nav>
+    </>
+  );
+}
+
+export default NavBar;
